a6fb22fcd516bbbbce47f31876001d0ddc19ea20
Pranav Padhye <prranavpadhye@gmail.com>
2025-12-02 23:26:14 +0530

    Uploaded Code for task1 and task2

---
+++ Task1.py
@@ -0,0 +1,16 @@
+# Takes two numbers as input from the user
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+# Performs the basic mathematical operations on these two numbers
+Addition_result        = first_number + second_number
+Substraction_result    = first_number - second_number
+Multiplication_result  = first_number * second_number
+Division_result        = first_number / second_number
+
+# Displays the results of each operation on the screen.
+print()
+print("Addition:", Addition_result)
+print("Substraction:", Substraction_result)
+print("Multiplication:", Multiplication_result)
+print("Division:", Division_result)
---
+++ Task2.py
@@ -0,0 +1,9 @@
+# Takes a user's first name and last name as input.
+first_name = input("Enter your first name: ")
+last_name  = input("Enter your last name: ")
+
+#Concatenates the first name and last name into a full name.
+full_name  = first_name + ' ' + last_name
+
+# Prints a personalized greeting message using the full name.
+print(f"Hello, {full_name}! Welcome to the python program.")
